fb042a65cc0ed7da6a429c78940ced4e8dba1be5
Aldo Portillo <94483081+aldoportillo@users.noreply.github.com>
2023-09-26 21:00:14 +0000

    Finish debugging omnicalc2

--- config/routes.rb
+++ config/routes.rb
@@ -10,10 +10,10 @@ Rails.application.routes.draw do
   get("/wizard_subtract", { :controller => "subtraction", :action => "subtract" })
 
   get("/multiply", { :controller => "multiplication", :action => "show_multiplication_form" })
-  
+
   get("/wizard_multiply", { :controller => "multiplication", :action => "multiply" })
 
   get("/divide", { :controller => "division", :action => "show_division_form" })
-  get("/wizard_div", { :controller => "div", :action => "divide_these" })
+  get("/wizard_divide", { :controller => "division", :action => "divide_these" })
 
 end
